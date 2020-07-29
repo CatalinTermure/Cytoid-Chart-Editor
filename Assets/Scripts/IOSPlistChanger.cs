4ffa68b95a1ba2d438176383f989776595982888
CatalinTermure <cataliin2002@gmail.com>
2020-07-29 15:48:57 +0300

    Added iOS Files app support.

---
+++ Assets/Scripts/IOSPlistChanger.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor.iOS.Xcode;
+using UnityEditor.Callbacks;
+using UnityEditor;
+using System.IO;
+
+public class IOSPlistChanger
+{
+    [PostProcessBuild]
+    public static void ChangeXCodePlist(BuildTarget target, string pathToBuiltProject)
+    {
+        if(target == BuildTarget.iOS)
+        {
+            //
+            string plistPath = Path.Combine(pathToBuiltProject, "Info.plist");
+            PlistDocument plist = new PlistDocument();
+            plist.ReadFromFile(plistPath);
+
+            PlistElementDict rootDict = plist.root;
+
+            rootDict.SetBoolean("UIFileSharingEnabled", true);
+            rootDict.SetBoolean("LSSupportsOpeningDocumentsInPlace", true);
+
+            File.WriteAllText(plistPath, plist.WriteToString());
+        }
+    }
+}
