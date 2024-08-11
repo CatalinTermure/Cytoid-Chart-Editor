ce1680de6948a90ae2991b9e0053ca91dec3eca0
CatalinTermure <cataliin2002@gmail.com>
2024-08-11 14:49:43 +0200

    Add search and load tests.

--- Assets/Editor/Tests/LevelLoadingTests.cs
+++ Assets/Editor/Tests/LevelLoadingTests.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.IO;
-using System.Linq;
 using CCE.Core;
+using CCE.LevelLoading;
 using NUnit.Framework;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
+using UnityEngine.UI;
 
 namespace CCE.Tests
 {
@@ -19,12 +21,12 @@ namespace CCE.Tests
             var levelsPath = GlobalState.Config.LevelStoragePath;
             if (!Directory.Exists(Path.Combine(levelsPath, "chovvy.test")))
             {
-                LoadSampleLevel(levelsPath);
+                ImportSampleLevel(levelsPath);
                 Assert.Fail("Sample level was not found and it was loaded. Please re-run the test.");
             }
         }
 
-        private static void LoadSampleLevel(string levelsPath)
+        private static void ImportSampleLevel(string levelsPath)
         {
             var sourcePath = Path.Combine(Application.dataPath, "Editor", "Resources", "chovvy.test");
             if (!Directory.Exists(sourcePath))
@@ -53,12 +55,58 @@ namespace CCE.Tests
             Assert.IsNotNull(levelList);
         }
 
-
         [Test]
         public void LevelListNotEmpty()
         {
             var levelList = GameObject.Find("Level List");
             Assert.Greater(levelList.transform.childCount, 0);
+        }
+
+        [UnityTest]
+        public IEnumerator SearchShowsSampleLevel()
+        {
+            var levelList = GameObject.Find("Level List").GetComponent<LevelList>();
+            levelList.Query("chovvy.test");
+
+            yield return null;
+
+            var levelNameObject = GameObject.Find("Level Name");
+            Assert.IsNotNull(levelNameObject);
+            var levelName = levelNameObject.GetComponent<Text>().text;
+            Assert.AreEqual("Night Detective", levelName);
+        }
+
+        [UnityTest]
+        public IEnumerator SearchSetsCurrentLevelID()
+        {
+            var levelList = GameObject.Find("Level List").GetComponent<LevelList>();
+            levelList.Query("chovvy.test");
+
+            yield return null;
+
+            var levelIDObject = GameObject.Find("Current Level ID");
+            Assert.IsNotNull(levelIDObject);
+            var levelID = levelIDObject.GetComponent<Text>().text;
+            Assert.AreEqual("chovvy.test", levelID);
+        }
+
+        [UnityTest]
+        public IEnumerator SampleLevelLoads()
+        {
+            var levelList = GameObject.Find("Level List").GetComponent<LevelList>();
+            levelList.Query("chovvy.test");
+
+            yield return new WaitForSeconds(1);
+
+            var extremeChartCard = GameObject.Find("Extreme Chart Card");
+            Assert.IsNotNull(extremeChartCard);
+            var actualChartCard = extremeChartCard.transform.Find("Actual Card");
+            var chartCardButton = actualChartCard.GetComponent<Button>();
+            chartCardButton.onClick.Invoke();
+
+            yield return null;
+
+            Assert.AreEqual("MainScene", SceneManager.GetActiveScene().name);
         }
     }
 }
