136cd5fcd8177abd2cd64392478f4a809e30f073
Chovvy <cataliin2002@gmail.com>
2025-10-20 20:15:48 +0300

    Make ToastMessageManager more efficient.

--- Assets/Scripts/Utils/ToastMessageManager.cs
+++ Assets/Scripts/Utils/ToastMessageManager.cs
@@ -7,30 +7,36 @@ namespace CCE.Utils
     ///     Class responsible for showing toast messages.
     ///     Must be attached to a <see cref="GameObject" /> with a <see cref="Text" /> component.
     /// </summary>
+    [RequireComponent(typeof(Text))]
     public class ToastMessageManager : MonoBehaviour
     {
         private float _toastEndTime = -1;
+        private Text _textComponent;
+
+        private void Awake()
+        {
+            _textComponent = gameObject.GetComponent<Text>();
+        }
 
         private void Update()
         {
-            if (_toastEndTime > 0)
+            if (_toastEndTime < 0) return;
+
+            if (_toastEndTime < Time.time)
             {
-                if (_toastEndTime < Time.time)
-                {
-                    gameObject.GetComponent<Text>().text = null;
-                    _toastEndTime = -1;
-                }
-                else
-                {
-                    var c = gameObject.GetComponent<Text>().color;
-                    gameObject.GetComponent<Text>().color = new Color(c.r, c.g, c.b, _toastEndTime - Time.time);
-                }
+                _textComponent.text = null;
+                _toastEndTime = -1;
+            }
+            else
+            {
+                var c = _textComponent.color;
+                _textComponent.color = new Color(c.r, c.g, c.b, _toastEndTime - Time.time);
             }
         }
 
         public void CreateToast(string toast, int toastDuration = 3)
         {
-            gameObject.GetComponent<Text>().text = toast;
+            _textComponent.text = toast;
             _toastEndTime = Time.time + toastDuration;
         }
     }
