ff03c0abdca0bba6ca14acc1fc4b63ffb97b8bfb
Chovvy <cataliin2002@gmail.com>
2025-12-27 15:57:21 +0200

    Move storyboard scene controller.

---
+++ Assets/Scripts/Storyboard/StoryboardSceneController.cs
@@ -0,0 +1,20 @@
+using CCE.Rendering.Notes;
+using UnityEngine;
+
+namespace CCE.Storyboard
+{
+    public class StoryboardSceneController : MonoBehaviour
+    {
+        [SerializeField] private NotePrefabs _notePrefabs;
+
+        void Start()
+        {
+            //
+        }
+
+        void Update()
+        {
+            //
+        }
+    }
+}
